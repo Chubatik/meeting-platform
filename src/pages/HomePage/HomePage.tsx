e6fba70e39414913b0eb7d24c5732a4b92865047
aostrovska <anna.ostrovska@wpp.com>
2022-01-22 21:21:13 +0200

    add home page style

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,6 +5,8 @@ import { v4 } from 'uuid'
 import { socket } from '../../socket'
 import { ACTIONS } from '../../socket/actions'
 
+import classes from './HomePage.module.scss'
+
 export const HomePage = () => {
   const navigate = useNavigate()
   const [rooms, setRooms] = useState([])
@@ -19,9 +21,9 @@ export const HomePage = () => {
     navigate(`/room/${v4()}`)
   }
   return (
-    <>
-      <h1>Available rooms</h1>
-
+    <div className={classes.textWrapper}>
+      <h1>Anonymous video calls now!</h1>
+      <h4>Consectetur sed dignissim id tellus vulputate. Egestas mi tristique ultricies interdum.</h4>
       <ul>
         {rooms.map((roomID) => (
           <li key={roomID}>
@@ -30,7 +32,17 @@ export const HomePage = () => {
           </li>
         ))}
       </ul>
-      <button onClick={clickHandler}>Create new room</button>
-    </>
+      <button onClick={clickHandler}>
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M12 6.5V12.5M12 12.5V18.5M12 12.5H18M12 12.5H6"
+            stroke="white"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+        </svg>
+        New meeting
+      </button>
+    </div>
   )
 }
